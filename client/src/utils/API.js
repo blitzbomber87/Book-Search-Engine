8513d67207fb76e8b0e1a15b70dfc78753dff389
Blitzbomber87 <elias.efly000@gmail.com>
2024-08-19 14:25:52 -0700

    Fixed files and file structure

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -18,7 +18,7 @@ export const createUser = (userData) => {
   });
 };
 
-export const loginUser = (userData) => {
+export const UserLogin = (userData) => {
   return fetch('/api/users/login', {
     method: 'POST',
     headers: {
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client';
+
+export const UserLogin = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+
+`;
+
